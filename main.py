4cfe4b6e51f20af3adb8ab0a8b909d678bbd07ab
RomaKarsakov <romakarsakov2008@mail.ru>
2025-03-14 17:30:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ def create_database():
     }
 
     conn = psycopg2.connect(**db_config)
-    cursor = conn.cursor()
 
     with open("karsakov_pletenev.sql", "r") as schema_file:
         schema_script = schema_file.read()
 
+    with conn.cursor() as cursor:
+        for st in schema_script.split(";"):
+            if st.strip():
+                e = cursor.execute(st.strip())
+                print(f"Executed: {e} ")
     conn.commit()
-    cursor.close()
-    conn.close()
+
 
     print("База данных и таблицы успешно созданы.")
 
